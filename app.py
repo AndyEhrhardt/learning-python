8ef0ebc75d7afd7d05b8688c0029a94c98ebd80b
Andrew Ehrhardt <aehrhardt@NXTM3NXT16.local>
2022-06-29 11:29:49 -0500

    string section complete

--- app.py
+++ app.py
@@ -1,1 +1,25 @@
-print("Hello World!")
+# print("Hello World!")
+# print("   /|")
+# print("  / |")
+# print(" /  |")
+# print("/___|")
+
+# character_name = "John"
+# character_age = "35"
+
+# print("There once was a man named" + character_name + ", ")
+# print("he was " + character_age + " years old")
+# print("He really liked the name " + character_name)
+# print("but didn't like being" + character_age)
+
+print("first line \nsecond line")
+print("escaping \"escaping\"")
+
+all_caps = "MF DOOM"
+print(all_caps.lower())
+print(all_caps.isupper())
+print(all_caps.lower().isupper())
+print(len(all_caps))
+print(all_caps[3])
+print(all_caps.index("O"))
+print(all_caps.replace("MF DOOM", "MAD VILLIAN"))
