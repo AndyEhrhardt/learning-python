334befb904ebc477812041f4bfb902fddbdae64e
Andrew Ehrhardt <aehrhardt@NXTM3NXT16.local>
2022-07-01 09:26:24 -0500

    basic input from user

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
+# Getting Started
+
 # print("Hello World!")
 # print("   /|")
 # print("  / |")
 # print(" /  |")
 # print("/___|")
+
+
+
+# Working With Strings
 
 # character_name = "John"
 # character_age = "35"
@@ -12,14 +18,70 @@
 # print("He really liked the name " + character_name)
 # print("but didn't like being" + character_age)
 
-print("first line \nsecond line")
-print("escaping \"escaping\"")
+# print("first line \nsecond line")
+# print("escaping \"escaping\"")
 
-all_caps = "MF DOOM"
-print(all_caps.lower())
-print(all_caps.isupper())
-print(all_caps.lower().isupper())
-print(len(all_caps))
-print(all_caps[3])
-print(all_caps.index("O"))
-print(all_caps.replace("MF DOOM", "MAD VILLIAN"))
+# all_caps = "MF DOOM"
+# print(all_caps.lower())
+# print(all_caps.isupper())
+# print(all_caps.lower().isupper())
+# print(len(all_caps))
+# print(all_caps[3])
+# print(all_caps.index("O"))
+# print(all_caps.replace("MF DOOM", "MAD VILLIAN"))
+
+
+
+
+# Working With Numbers
+
+from math import *
+
+# print(3 * (4 + 5))
+# print(10 % 3)
+
+# my_num = 5
+# print(my_num)
+# print(str(my_num) + " my favorite number")
+# print(5 + "will this throw an error, I think it will")
+
+# another_num = -5
+# print(abs(another_num))
+
+# print(pow(3, 5))
+# print(max(4,10))
+# print(min(4,10))
+# print(round(3.4))
+
+# print(floor(3.7))
+# print(ceil(3.7))
+# print(sqrt(36))
+
+
+
+# Getting Input from Users
+
+# name = input("Enter your name: ")
+# age = input("Enter your age: ")
+# print("Hello " + name + " you are " + age)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
