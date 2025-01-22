bfccd0ca350f01e5928fdbff1b9269f9338847f0
Andy Ehrhardt <49926696+AndyEhrhardt@users.noreply.github.com>
2025-01-22 10:24:49 -0500

    Update leetCode.py

--- leetCode.py
+++ leetCode.py
@@ -93,3 +93,50 @@ class Solution(object):
             elif (char == 'M'):
                 total += 1000
         return total
+
+# 14. Longest Common Prefix
+# Write a function to find the longest common prefix string amongst an array of strings.
+
+# If there is no common prefix, return an empty string "".
+
+ 
+
+# Example 1:
+
+# Input: strs = ["flower","flow","flight"]
+# Output: "fl"
+# Example 2:
+
+# Input: strs = ["dog","racecar","car"]
+# Output: ""
+# Explanation: There is no common prefix among the input strings.
+ 
+
+# Constraints:
+
+# 1 <= strs.length <= 200
+# 0 <= strs[i].length <= 200
+# strs[i] consists of only lowercase English letters if it is non-empty.
+
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        shortestString = 0
+        for string in strs:
+            if(len(string) > shortestString):
+                shortestString = len(string)
+        longestPrefix = ''
+        for i in range(shortestString):
+            subStrArr = []
+            for checkString in strs:      
+                subStrArr.append(checkString[:i + 1])          
+            if (len(set(subStrArr)) == 1):
+                longestPrefix = subStrArr[0]
+            else:
+                break
+        return longestPrefix
+
+
