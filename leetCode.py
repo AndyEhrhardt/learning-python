4b2efedb0ebd0806f5b63028fddd06a3d2eedad5
Andy Ehrhardt <49926696+AndyEhrhardt@users.noreply.github.com>
2025-01-23 08:34:41 -0500

    Update leetCode.py

--- leetCode.py
+++ leetCode.py
@@ -239,5 +239,21 @@ class Solution(object):
                 placeHolder += 1
         return placeHolder
 
-            
+# 27. Remove Element
+
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        k = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+        return k
+        
+
 
