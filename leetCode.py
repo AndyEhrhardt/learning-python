c42cc56b962414e4cb12969db3de7dfa86f035ea
Andy Ehrhardt <49926696+AndyEhrhardt@users.noreply.github.com>
2025-01-25 15:19:44 -0600

    Update leetCode.py

--- leetCode.py
+++ leetCode.py
@@ -301,3 +301,20 @@ class Solution(object):
         """
         sArr = s.split()
         return len(sArr[len(sArr) - 1])
+
+# 66. Plus One
+
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        numOfNines = 0
+        for i in range(len(digits) - 1, -1, -1):
+            if digits[i] != 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
+            if i == 0:
+                return [1] + digits
